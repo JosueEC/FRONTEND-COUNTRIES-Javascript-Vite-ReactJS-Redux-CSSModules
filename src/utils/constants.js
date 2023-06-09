df83c18334eda9320ad742d50f7ab0cfd5406297
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-08 20:19:07 -0600

    feat(utils/constants): :hammer: Creation of the ROUTES and API constants.

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+const ROUTES = Object.freeze({
+  LANDING: '/',
+  HOME: '/home',
+  DETAIL: '/details/:countryID',
+  FORM: '/form'
+})
+
+const API = Object.freeze({
+  DOMAIN: 'http://localhost:3001'
+})
+
+export {
+  ROUTES,
+  API
+}
