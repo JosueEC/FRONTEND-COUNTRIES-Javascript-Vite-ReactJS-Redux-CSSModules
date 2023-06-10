2fa42d343c6a616f632a9d8359ce8bfe6a87a211
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-09 21:23:51 -0600

    feat(components): :hammer: Tested the API responses with the CardCountry component.

--- src/components/CardCountry/CardCountry.jsx
+++ src/components/CardCountry/CardCountry.jsx
@@ -1,22 +1,47 @@
 import { Link } from 'react-router-dom'
 import styles from './CardCountry.module.css'
+import { useEffect, useState } from 'react'
 
 export default function CardCountry () {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3001/countries/COL')
+      .then((response) => {
+        if (!response.ok) throw new Error(`Error HTTP: ${response.status}`)
+        return response.json()
+      })
+      .then((results) => {
+        console.info('fetch-country-card')
+        setCountry(results.data)
+      })
+      .catch((error) => console.error(error.message))
+  }, [])
+
   return (
     <Link to='/detail/MEX' className={styles.linkCard}>
-      <div className={styles.card}>
-        <div className={styles.containerImage}>
-          <img src='https://flagcdn.com/w320/mx.png' alt='country flag' />
-        </div>
-        <div className={styles.content}>
-          <h3 className={styles.title}>Mexico</h3>
-          <p><span>Region:</span>americas</p>
-          <p><span>Capital:</span>mexico city</p>
-          <p><span>Population:</span>128932753</p>
-          <p><span>Continent:</span>north america</p>
-          <button>Click to see more details...</button>
-        </div>
-      </div>
+      {
+      country
+        ? (
+          <div className={styles.card}>
+            <div className={styles.containerImage}>
+              <img src={country.image} alt='country flag' />
+            </div>
+            <div className={styles.content}>
+              <h3 className={styles.title}>Mexico</h3>
+              <p><span>Region:</span>americas</p>
+              <p><span>Capital:</span>mexico city</p>
+              <p><span>Population:</span>128932753</p>
+              <p><span>Continent:</span>north america</p>
+              <button>Click to see more details...</button>
+            </div>
+          </div>
+
+          )
+        : (
+          <h2>Loading...</h2>
+          )
+    }
     </Link>
   )
 }
--- src/services/redux/store.js
+++ src/services/redux/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducer'
-import { ThunkMiddleware } from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk'
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   rootReducer,
-  composeEnhancer(applyMiddleware(ThunkMiddleware))
+  composeEnhancer(applyMiddleware(thunkMiddleware))
 )
 
 export default store
