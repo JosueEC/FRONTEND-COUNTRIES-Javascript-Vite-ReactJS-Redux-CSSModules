3b4a7b4dcaf714f2295015856d28dfe5814a1d89
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-12 22:16:28 -0600

    feat(components): :sparkles: Improved visual user experience with the Paginator component.

--- src/components/Paginator/Paginator.jsx
+++ src/components/Paginator/Paginator.jsx
@@ -1,33 +1,54 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react'
 import './Paginator.css'
 
 export default function Paginator ({ totalPages, pages, currentPage, setCurrentPage }) {
+  const pageNumberLimit = 5
+  const [maxNumberPage, setMaxNumberPage] = useState(5)
+  const [minNumberPage, setMinNumberPage] = useState(0)
+
   function handlePreviousPage () {
-    if (currentPage !== 1) setCurrentPage(currentPage - 1)
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1)
+      if (Number((currentPage - 1) % pageNumberLimit) === 0) {
+        setMaxNumberPage(maxNumberPage - pageNumberLimit)
+        setMinNumberPage(minNumberPage - pageNumberLimit)
+      }
+    }
   }
 
   function handleNextPage () {
-    if (currentPage !== totalPages) setCurrentPage(currentPage + 1)
+    if (currentPage !== totalPages) {
+      setCurrentPage(currentPage + 1)
+      if (currentPage + 1 > maxNumberPage) {
+        setMaxNumberPage(maxNumberPage + pageNumberLimit)
+        setMinNumberPage(minNumberPage + pageNumberLimit)
+      }
+    }
   }
 
   return (
     <div className='containerPaginator'>
-      <span onClick={handlePreviousPage}>Previous</span>
+      <span onClick={handlePreviousPage} className='paginatorItem'>{'<'}</span>
       <div>
         {
         pages.map((page) => {
-          return (
-            <span
-              key={page}
-              onClick={() => setCurrentPage(page)}
-              className={`${currentPage === page ? 'active' : ''}`}
-            >{`${page} | `}
-            </span>
-          )
+          if (page < maxNumberPage + 1 && page > minNumberPage) {
+            return (
+              <button
+                key={page}
+                onClick={() => setCurrentPage(page)}
+                className={`${currentPage === page ? 'paginatorItem paginatorItem--active' : 'paginatorItem'}`}
+              >{page}
+              </button>
+            )
+          } else {
+            return null
+          }
         })
       }
       </div>
-      <span onClick={handleNextPage}>Next</span>
+      <span onClick={handleNextPage} className='paginatorItem'>{'>'}</span>
     </div>
   )
 }
