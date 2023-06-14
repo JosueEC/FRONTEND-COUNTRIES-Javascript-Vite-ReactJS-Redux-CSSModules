2acfba87aead15ffeb7dcaa3f71692034d90f80e
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-14 13:45:42 -0600

    feat(pages): :hammer: Creation of the validation fields in the FormNewActivity Page.

--- src/pages/FormNewActivity/FormNewActivity.jsx
+++ src/pages/FormNewActivity/FormNewActivity.jsx
@@ -1,5 +1,5 @@
 import InputValidate from '../../components/InputValidate/InputValidate'
-import { validateActivityName } from '../../services/validators/formValidators'
+import { validateActivityName, validateDifficultyActivity, validateActivityDuration, validateActivitySeason, validateActivityImage } from '../../services/validators/formValidators'
 
 import styles from './FormNewActivity.module.css'
 
@@ -9,7 +9,10 @@ export default function FormNewActivity () {
       <form className={styles.form}>
         <div className={styles.containerInputs}>
           <InputValidate inputType='text' nameLabel='name' textLabel='Activity Name' validateFunction={validateActivityName} />
-          <InputValidate inputType='password' nameLabel='difficulty' textLabel='Difficulty' validateFunction={validateActivityName} />
+          <InputValidate inputType='number' nameLabel='difficulty' textLabel='Difficulty' validateFunction={validateDifficultyActivity} />
+          <InputValidate inputType='number' nameLabel='duration' textLabel='Duration' validateFunction={validateActivityDuration} />
+          <InputValidate inputType='text' nameLabel='season' textLabel='Season' validateFunction={validateActivitySeason} />
+          <InputValidate inputType='text' nameLabel='image' textLabel='Image' validateFunction={validateActivityImage} />
         </div>
       </form>
     </section>
--- src/services/validators/formValidators.js
+++ src/services/validators/formValidators.js
@@ -7,6 +7,44 @@ const validateActivityName = (activityName, setErrorData) => {
   }
 }
 
+const validateDifficultyActivity = (difficulty, setErrorData) => {
+  if (difficulty < 1 || difficulty > 5) {
+    setErrorData('activity difficulty must be between 1 and 5 range.')
+  } else {
+    setErrorData('')
+  }
+}
+
+const validateActivityDuration = (duration, setErrorData) => {
+  if (duration < 1 || duration > 5) {
+    setErrorData('activity duration must be less than equal to 5 hours.')
+  } else {
+    setErrorData('')
+  }
+}
+
+const validateActivitySeason = (season, setErrorData) => {
+  const acceptedSeasons = ['verano', 'otoño', 'invierno', 'primavera']
+  if (!acceptedSeasons.includes(season)) {
+    setErrorData('activity season must be a valid season of the year.')
+  } else {
+    setErrorData('')
+  }
+}
+
+const validateActivityImage = (imageURL, setErrorData) => {
+  const regexURL = /^(https?:\/\/)/
+  if (!regexURL.test(imageURL)) {
+    setErrorData('invalid URL format, must be an URL image')
+  } else {
+    setErrorData('')
+  }
+}
+
 export {
-  validateActivityName
+  validateActivityName,
+  validateDifficultyActivity,
+  validateActivityDuration,
+  validateActivitySeason,
+  validateActivityImage
 }
