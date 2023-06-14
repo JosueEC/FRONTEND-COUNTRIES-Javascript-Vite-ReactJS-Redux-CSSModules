3ea7e37a3255257f2b109ad025bc3274bcdfb637
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-14 17:54:40 -0600

    feat(components): :sparkles: Integration of the component SelectorCountry with the getFlags API endpoint.

--- src/components/SelectorBox/SelectorBox.jsx
+++ src/components/SelectorBox/SelectorBox.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react'
 import './SelectorBox.css'
 
-export default function SelectorBox () {
+export default function SelectorBox ({ imageFlag, countryName }) {
   const [selected, setSelected] = useState(false)
 
   function handleSelected () {
@@ -10,8 +11,8 @@ export default function SelectorBox () {
 
   return (
     <div className={(selected) ? 'box-selector box-selector--selected' : 'box-selector'} onClick={handleSelected}>
-      <img src='https://flagcdn.com/w320/mx.png' alt='mexico flag' />
-      <span>Mexico</span>
+      <img src={imageFlag} alt={`${countryName} flag`} />
+      <span>{countryName}</span>
     </div>
   )
 }
--- src/components/SelectorCountry/SelectorCountry.jsx
+++ src/components/SelectorCountry/SelectorCountry.jsx
@@ -1,11 +1,43 @@
+import { useState, useEffect } from 'react'
+import { API } from '../../utils/constants'
 import SelectorBox from '../SelectorBox/SelectorBox'
 import './SelectorCountry.css'
 
 export default function SelectorCountry () {
+  const [countryIDS, setCountryIDS] = useState([])
+
+  useEffect(() => {
+    fetch(`${API.DOMAIN}/countries/all/flags`)
+      .then((response) => {
+        if (!response.ok) throw new Error(`Error HTTP: ${response.status}`)
+        return response.json()
+      })
+      .then((results) => {
+        console.info('fetch-all-country-IDs')
+        setCountryIDS(results.data)
+      })
+      .catch((error) => console.error(error.message))
+  }, [])
+
   return (
     <div className='containerOptions'>
-      <SelectorBox />
-      <SelectorBox />
+      {
+        (countryIDS)
+          ? (
+              countryIDS.map(({ id, name, image }) => {
+                return (
+                  <SelectorBox
+                    key={id}
+                    imageFlag={image}
+                    countryName={name}
+                  />
+                )
+              })
+            )
+          : (
+            <p>Loading Flags</p>
+            )
+      }
     </div>
   )
 }
