834347101baa89844571f8098139febe363dc706
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-17 14:23:08 -0600

    feat(components): :lipstick: Addition of the mobile CSS style to the SearchBar component.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import SearchBar from '../../components/SearchBar/SearchBar'
 import OrderContainer from '../../components/OrderContainer/OrderContainer'
 import ContainerCards from '../../components/ContainerCards/ContainerCards'
+import './Home.css'
 
 export default function Home () {
   return (
